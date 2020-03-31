cad0fc2be9599ac04eb27e7bf911571a5b092427
Ahad Islam <ahad.islam624@gmail.com>
2020-03-31 16:56:49 -0400

    creating a list is fun and easy

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,7 +11,25 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Section(header: Text("Section 1")) {
+                Text("Static row 1")
+                Text("Static row 2")
+                Text("Static row 3")
+            }
+            
+            Section(header: Text("Section 2")) {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+            
+            Section(header: Text("Section 3")) {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        }
+    .listStyle(GroupedListStyle())
     }
 }
 
